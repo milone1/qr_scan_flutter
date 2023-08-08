8b5739c6ba7d3c64a3f49b9104f0e383dc9db028
Erick Millan <erickmilan1002@gmail.com>
2023-08-08 11:38:45 -0500

    delete apikey from ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyASluFjjuW300WKgGJhim7PDxh813PASJU")
+    // ApiKEY HEre
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
